655780c30aac7703c96921580a1e69c12a2e3214
Fathi Rabbani <fathirabbani2707@gmail.com>
2019-04-12 22:26:13 +0700

    update Teori Fix

---
+++ src/fathi/chapter7/code.py
@@ -0,0 +1,17 @@
+import numpy as np 
+
+arr1 = [1, -3, 15, -466] 
+print ("Absolute Value of arr1 : \n", 
+    np.absolute(arr1)) 
+
+arr2 = [23 , -56] 
+print ("\nAbsolute Value of arr2 : \n", 
+    np.absolute(arr2))
+
+#hasilnya adalah sebagai berikut :
+
+Absolute Value of arr1 : 
+ [  1   3  15 466]
+
+Absolute Value of arr2 : 
+ [23 56]
